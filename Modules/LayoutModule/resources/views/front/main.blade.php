d907cd04df73114e98405de118837541ae6fd9a6
Mohamed Dwidar <mdwidar84@gmail.com>
2026-09-26 14:56:42 +0300

    test

--- Modules/LayoutModule/resources/views/front/main.blade.php
+++ Modules/LayoutModule/resources/views/front/main.blade.php
@@ -73,7 +73,18 @@
     <!-- End Vertical Ad Rails -->
 
     @include('layoutmodule::front.header')
-
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-0462453958685277"
+     crossorigin="anonymous"></script>
+<!-- Eg-Doctor - Doctor - Display -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-0462453958685277"
+     data-ad-slot="9007900471"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
     <!-- Start Page Hero -->
     <section class="egd-page-hero">
         <div class="container">
